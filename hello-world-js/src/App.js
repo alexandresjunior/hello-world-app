76a9362dc749eb02e5067da5fee1fad14de04d3b
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-07-28 06:57:42 -0300

    Add button to call backend service

--- hello-world-js/src/App.js
+++ hello-world-js/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { sayHello } from './service';
 
 function App() {
   const [name, setName] = useState([]);
   const [greetings, setGreetings] = useState("Type your name here:");
+
+  const handleName = () => {
+    sayHello(`/hello?name=${name}`, setGreetings);
+  }
 
   return (
     <div className="App">
@@ -12,6 +17,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>{greetings}</p>
         <input className="App-input" type="text" onChange={(event) => setName(event.target.value)}/>
+        <button className="App-button" onClick={() => handleName()}>Enviar</button>
       </header>
     </div>
   );
