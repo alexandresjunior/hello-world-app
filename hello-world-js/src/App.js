68e37e19f352f8cd56a1747f3ca603f081de53d5
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-07-28 06:54:21 -0300

    Add input field for name

--- hello-world-js/src/App.js
+++ hello-world-js/src/App.js
@@ -10,17 +10,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{greetings}</p>
+        <input className="App-input" type="text" onChange={(event) => setName(event.target.value)}/>
       </header>
     </div>
   );
