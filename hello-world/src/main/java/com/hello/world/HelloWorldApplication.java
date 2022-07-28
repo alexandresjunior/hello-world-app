fecd54c1cfe2d4ffc42e6ef58067e3f1506a88d4
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-07-28 06:58:27 -0300

    Resolve CORS policy issue

--- hello-world/src/main/java/com/hello/world/HelloWorldApplication.java
+++ hello-world/src/main/java/com/hello/world/HelloWorldApplication.java
@@ -2,12 +2,14 @@ package com.hello.world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
